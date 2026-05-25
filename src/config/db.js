c9ad66cad28d1eb09d1f1e0cda12ffd0e561a750
Mr.Kumar Karan <k.mathur9870@gmail.com>
2026-05-25 21:32:51 +0530

    Add MongoDB dependency and enhance database connection logic and check api authntication working successfully

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
+import dns from "dns";
 
 const connectDB = async () =>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_DB_URL);
+        const mongoUrl = process.env.MONGO_DB_URL;
+        if (mongoUrl?.startsWith("mongodb+srv://")) {
+            const servers = dns.getServers();
+            if (servers.length === 1 && servers[0] === "127.0.0.1") {
+                dns.setServers(["8.8.8.8", "8.8.4.4"]);
+            }
+        }
+
+        const conn = await mongoose.connect(mongoUrl, { family: 4 });
         console.log("Databse connected successfully");
     }catch(error){
         console.log(`Error ${error.message}`);
